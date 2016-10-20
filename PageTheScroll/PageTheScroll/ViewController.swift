d3eaf0da235f8a11e1194eef99c5bc5f9c119f95
Shashank Rao <shashank.rao@oracle.com>
2016-10-20 01:47:29 -0700

    working scrollview left to right

--- PageTheScroll/PageTheScroll/ViewController.swift
+++ PageTheScroll/PageTheScroll/ViewController.swift
@@ -16,6 +16,11 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated);
         var contentWidth: CGFloat = 0.0
         for x in 0...2{
             let image = UIImage(named:"icon\(x).png")
@@ -24,17 +29,16 @@ class ViewController: UIViewController {
             
             
             var newX: CGFloat = 0.0
-            newX = view.frame.midX + view.frame.size.width * CGFloat(x)
+            newX = scrollView.frame.size.width/2 + scrollView.frame.size.width * CGFloat(x)
             contentWidth += newX
             scrollView.addSubview(imageView)
-            imageView.frame = CGRect(x:newX-75,y:(view.frame.size.height/2)-75,width:150,height:150)
+            imageView.frame = CGRect(x:newX-75,y:(scrollView.frame.size.height/2)-75,width:150,height:150)
             
         }
         
+        //scrollView.backgroundColor = UIColor.purple
         scrollView.contentSize = CGSize(width:contentWidth, height:view.frame.size.height)
-        
-        
-        
+        scrollView.clipsToBounds=false;
     }
  
 
